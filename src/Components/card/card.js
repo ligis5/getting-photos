7109d213f4ec0e6a46b3e5354bd3fd4b987fe36d
Ligis5 <lukasnor5@gmail.com>
2021-03-10 22:05:47 +0200

    finished

--- src/Components/card/card.js
+++ src/Components/card/card.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import './card.css'
+import React, { useRef } from "react";
+import "./card.css";
+import useObserver from "../../Observer";
 
-export const Card = (props) => {
-    return (
-        <div className='inner-card'>
-         <img className= 'inner-img' alt='monster' src={props.image} />
-         <h3 className='h1-css'>Author:{props.name}</h3>
-        </div>
-    )
-}
+export const Card = ({ image, name, imageLink }) => {
+  const ref = useRef();
+  const onScreen = useObserver(ref);
+
+  return (
+    <div ref={ref} className="inner-card">
+      {onScreen ? (
+        <>
+          <a href={imageLink} target="_blank" rel="noreferrer">
+            <img
+              loading="lazy"
+              className="inner-img"
+              alt="monster"
+              src={image}
+            />
+          </a>
+          <h3 className="h1-css">Author:{name}</h3>
+        </>
+      ) : (
+        <h1>...Loading</h1>
+      )}
+    </div>
+  );
+};
---
+++ src/Observer.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+const useObserver = (ref) => {
+  const [Intersecting, setIntersecting] = useState(false);
+
+  const options = {
+    rootMargin: "0px 0px -100px",
+  };
+
+  const observer = new IntersectionObserver(([entry]) => {
+    if (!entry.isIntersecting) {
+      return;
+    }
+    setIntersecting(entry.isIntersecting);
+    observer.unobserve(entry.target);
+  }, options);
+
+  useEffect(() => {
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return Intersecting;
+};
+export default useObserver;
